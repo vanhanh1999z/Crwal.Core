d83e4b279322dfcee42e89989cc05418d8f10e4b
vanhanh1999z <hl.gaukl@gmail.com>
2023-02-14 17:24:58 +0700

    udp

--- Crwal.Core/Sql/ConnectionString.cs
+++ Crwal.Core/Sql/ConnectionString.cs
@@ -5,43 +5,20 @@ namespace Crwal.Core.Sql
 {
     public class ConnectionString
     {
-        internal static string FOLDER_CONFIX = @"C:\CrawlerTeam_Config";
-
-        internal ConnectionString()
+        public string ServerName { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public ConnectionString(string serverName, string user, string password)
         {
+            this.ServerName = serverName;
+            this.User = user;
+            this.Password = password;
         }
 
-        /// <summary>
-        /// Đọc File Config theo file
-        /// </summary>
-        /// <param name="filename">ví dụ: ConnectionString_News_DbLocal.txt</param>
-        /// <returns></returns>
-        internal static string GetConnectString(string filename)
+        public string BuildConnectionString()
         {
-            string conn = "";
-
-            string fileConfig = FOLDER_CONFIX + @"\" + filename;
-
-            FileReadWrite f = new FileReadWrite(fileConfig);
-
-            List<string> lis = f.GetListLine();
-
-            if (lis != null && lis.Count > 0)
-            {
-                conn = lis[0];
-            }
-
-            return conn;
-        }
-
-        /// <summary>
-        /// Lấy chuỗi kết nối tới database trên server local - lưu theo đường dẫn C:\CrawlerTeam_Config\
-        /// Nếu chưa có file này ConnectionString_Soccer.txt, hãy tạo 1 file rồi đặt nội dung này Data Source=.;Initial Catalog=SoccerDb;User ID=coder;Password=coder, vào đó nhé. Tùy từng SQL Server mà đổi chuỗi kết nối cho đúng
-        /// </summary>
-        /// <returns>Data Source=.;Initial Catalog=SoccerDb;User ID=coder;Password=coder</returns>
-        internal static string GetConnectString_SoccerDb()
-        {
-            return GetConnectString("ConnectionString_Soccer.txt");
+            string dePass = StringCipher.Decrypt(Password);
+            return $"Data Source={ServerName};Initial Catalog = social_index_v2; User ID = {User}; Password = {dePass}";
         }
     }
 }
